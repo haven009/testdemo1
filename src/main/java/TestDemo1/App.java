511835d553ff2d5c28e5ca3750057040d294d8f1
haven <13021093200@163.com>
2017-09-30 20:52:45 +0800

    Update App.java

--- src/main/java/TestDemo1/App.java
+++ src/main/java/TestDemo1/App.java
@@ -21,6 +21,7 @@ public class App {
         User user2 = new User(2, "xiaohong", "24");
 //        List<String> testList = new ArrayList<String>();
         List<Map> testList = new ArrayList<Map>();
+        //        List<String> testList = new ArrayList<String>();
         Map<String, Object> testMap1 = new HashMap<String, Object>();
         Map<String, Object> testMap2 = new HashMap<String, Object>();
         testMap1.put("id", user1.getId());
@@ -30,6 +31,7 @@ public class App {
         testMap2.put("name", user2.getName());
         testMap2.put("sex", user2.getSex());
         testList.add(testMap1);
+        //        List<String> testList = new ArrayList<String>();
         testList.add(testMap2);
         jedis.set("my1".getBytes(), SerializeUtil.serialize(testList));
         System.out.println(SerializeUtil.unserialize(jedis.get("my1".getBytes())));
